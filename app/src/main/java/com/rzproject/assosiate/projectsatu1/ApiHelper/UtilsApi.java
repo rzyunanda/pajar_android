d3ec64c2e0be1b2d4b87864f1924dd73964066b3
Rezki Yunanda <38293894+rzyunanda@users.noreply.github.com>
2020-06-02 13:45:45 +0700

    Update UtilsApi.java

--- app/src/main/java/com/rzproject/assosiate/projectsatu1/ApiHelper/UtilsApi.java
+++ app/src/main/java/com/rzproject/assosiate/projectsatu1/ApiHelper/UtilsApi.java
@@ -2,7 +2,7 @@ package com.rzproject.assosiate.projectsatu1.ApiHelper;
 
 public class UtilsApi {
 
-    public static final String BASE_URL_API = "https://vast-reef-07673.herokuapp.com/";
+    public static final String BASE_URL_API = "https://vast-reef-07673.herokuapp.com/api";
 
     public static BaseApiService getAPIService() {
         return RetrofitClient.getClient(BASE_URL_API).create(BaseApiService.class);
